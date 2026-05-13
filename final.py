c7ff4701fbc29005673354232363ea58b9fe2e09
Nikhil Justin <nikhiljustin2006@gmail.com>
2026-05-13 19:28:49 -0400

    created main function andasked for input to create player variables and start the game

--- final.py
+++ final.py
@@ -1,1 +1,9 @@
-print("Testing one more time")
+
+
+def main():
+    print("Welcome to Tuple Out!")
+    player1 = input("Enter name for Player 1: ")
+    player2 = input("Enter name for Player 2: ")
+    print(f"\nOk {player1} and {player2}, let's begin!")
+
+main()
