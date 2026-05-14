acde8e6f5978d1833084254957a69da5323cfce3
Nikhil Justin <nikhiljustin2006@gmail.com>
2026-05-14 18:49:25 -0400

    created roll_dice function with random number generator and is_tuple_out for 0 points. Creating matched dice function so fixed dice are not rolled again

--- final.py
+++ final.py
@@ -1,7 +1,4 @@
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 def main():
     print("Welcome to Tuple Out!")
@@ -10,3 +7,31 @@ def main():
     print(f"\nOk {player1} and {player2}, let's begin!")
 
 main()
+
+def roll_dice():
+    return list(np.random.randint(1, 7, size = 3))
+
+def is_tuple_out():
+    return dice[0] == dice[1] == dice[2]
+
+def get_fixed_dice():
+    
+    fixed = [False, False, False]
+
+    if dice[0] == dice[1]:
+        fixed[0] = True
+        fixed[1] = True
+
+    if dice[0] == dice[2]:
+        fixed[0] = True
+        fixed[2] = True
+
+    if dice[1] == dice[2]:
+        fixed[1] = True
+        fixed[2] = True
+    
+    return fixed
+
+print(roll_dice())
+
+dice = roll_dice()
